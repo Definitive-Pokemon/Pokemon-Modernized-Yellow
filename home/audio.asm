1c7705ce6734d0251f387a75c1dbcd2dd2f765f6
ElwinBran <elwinbran@gmail.com>
2020-09-24 20:03:46 +0200

    Test music

--- home/audio.asm
+++ home/audio.asm
@@ -1,296 +1,163 @@
-PlayDefaultMusic::
-	call WaitForSoundToFinish
-	xor a
-	ld c, a
-	ld d, a
-	ld [wLastMusicSoundID], a
-	jr PlayDefaultMusicCommon
+INCLUDE "constants.asm"
 
-PlayDefaultMusicFadeOutCurrent::
-; Fade out the current music and then play the default music.
-	ld c, 10
-	ld d, 0
-	ld a, [wd72e]
-	bit 5, a ; has a battle just ended?
-	jr z, PlayDefaultMusicCommon
-	xor a
-	ld [wLastMusicSoundID], a
-	ld c, 8
-	ld d, c
 
-PlayDefaultMusicCommon::
-	ld a, [wWalkBikeSurfState]
-	and a
-	jr z, .walking
-	cp $2
-	jr z, .surfing
-	call CheckForNoBikingMusicMap
-	jr c, .walking
-	ld a, MUSIC_BIKE_RIDING
-	jr .next
+SECTION "Audio", ROMX
 
-.surfing
-	ld a, MUSIC_SURFING
+INCLUDE "audio/engine.asm"
+INCLUDE "data/trainers/encounter_music.asm"
+INCLUDE "audio/music_pointers.asm"
+INCLUDE "audio/music/nothing.asm"
+INCLUDE "audio/cry_pointers.asm"
+INCLUDE "audio/sfx_pointers.asm"
 
-.next
-	ld b, a
-	ld a, d
-	and a ; should current music be faded out first?
-	ld a, BANK(Music_BikeRiding)
-	jr nz, .next2
 
-; Only change the audio ROM bank if the current music isn't going to be faded
-; out before the default music begins.
-	ld [wAudioROMBank], a
+SECTION "Songs 1", ROMX
 
-.next2
-; [wAudioSavedROMBank] will be copied to [wAudioROMBank] after fading out the
-; current music (if the current music is faded out).
-	ld [wAudioSavedROMBank], a
-	jr .next3
+INCLUDE "audio/music/route36.asm"
+INCLUDE "audio/music/rivalbattle.asm"
+INCLUDE "audio/music/rocketbattle.asm"
+INCLUDE "audio/music/elmslab.asm"
+INCLUDE "audio/music/darkcave.asm"
+INCLUDE "audio/music/johtogymbattle.asm"
+INCLUDE "audio/music/championbattle.asm"
+INCLUDE "audio/music/ssaqua.asm"
+INCLUDE "audio/music/newbarktown.asm"
+INCLUDE "audio/music/goldenrodcity.asm"
+INCLUDE "audio/music/vermilioncity.asm"
+INCLUDE "audio/music/titlescreen.asm"
+INCLUDE "audio/music/ruinsofalphinterior.asm"
+INCLUDE "audio/music/lookpokemaniac.asm"
+INCLUDE "audio/music/trainervictory.asm"
 
-.walking
-	ld a, [wMapMusicSoundID]
-	ld b, a
-	call CompareMapMusicBankWithCurrentBank
-	jr c, .next4
 
-.next3
-	ld a, [wLastMusicSoundID]
-	cp b ; is the default music already playing?
-	ret z ; if so, do nothing
+SECTION "Songs 2", ROMX
 
-.next4
-	ld a, c
-	ld [wAudioFadeOutControl], a
-	ld a, b
-	ld [wLastMusicSoundID], a
-	ld [wNewSoundID], a
-	jp PlaySound
+INCLUDE "audio/music/route1.asm"
+INCLUDE "audio/music/route3.asm"
+INCLUDE "audio/music/route12.asm"
+INCLUDE "audio/music/kantogymbattle.asm"
+INCLUDE "audio/music/kantotrainerbattle.asm"
+INCLUDE "audio/music/kantowildbattle.asm"
+INCLUDE "audio/music/pokemoncenter.asm"
+INCLUDE "audio/music/looklass.asm"
+INCLUDE "audio/music/lookofficer.asm"
+INCLUDE "audio/music/route2.asm"
+INCLUDE "audio/music/mtmoon.asm"
+INCLUDE "audio/music/showmearound.asm"
+INCLUDE "audio/music/gamecorner.asm"
+INCLUDE "audio/music/bicycle.asm"
+INCLUDE "audio/music/looksage.asm"
+INCLUDE "audio/music/pokemonchannel.asm"
+INCLUDE "audio/music/lighthouse.asm"
+INCLUDE "audio/music/lakeofrage.asm"
+INCLUDE "audio/music/indigoplateau.asm"
+INCLUDE "audio/music/route37.asm"
+INCLUDE "audio/music/rockethideout.asm"
+INCLUDE "audio/music/dragonsden.asm"
+INCLUDE "audio/music/ruinsofalphradio.asm"
+INCLUDE "audio/music/lookbeauty.asm"
+INCLUDE "audio/music/route26.asm"
+INCLUDE "audio/music/ecruteakcity.asm"
+INCLUDE "audio/music/lakeofragerocketradio.asm"
+INCLUDE "audio/music/magnettrain.asm"
+INCLUDE "audio/music/lavendertown.asm"
+INCLUDE "audio/music/dancinghall.asm"
+INCLUDE "audio/music/contestresults.asm"
+INCLUDE "audio/music/route30.asm"
 
-CheckForNoBikingMusicMap::
-; probably used to not change music upon getting on bike
-	ld a, [wCurMap]
-	cp ROUTE_23
-	jr z, .found
-	cp VICTORY_ROAD_1F
-	jr z, .found
-	cp VICTORY_ROAD_2F
-	jr z, .found
-	cp VICTORY_ROAD_3F
-	jr z, .found
-	cp INDIGO_PLATEAU
-	jr z, .found
-	and a
-	ret
-.found
-	scf
-	ret
 
-UpdateMusic6Times::
-	ld c, 6
-UpdateMusicCTimes::
-.loop
-	push bc
-	push hl
-	callba Audio1_UpdateMusic
-	pop hl
-	pop bc
-	dec c
-	jr nz, .loop
-	ret
+SECTION "Songs 3", ROMX
 
-CompareMapMusicBankWithCurrentBank::
-; Compares the map music's audio ROM bank with the current audio ROM bank
-; and updates the audio ROM bank variables.
-; Returns whether the banks are different in carry.
-	ld a, [wMapMusicROMBank]
-	ld e, a
-	ld a, [wAudioROMBank]
-	cp e
-	jr nz, .differentBanks
-	ld [wAudioSavedROMBank], a
-	and a
-	ret
-.differentBanks
-	ld a, c ; this is a fade-out counter value and it's always non-zero
-	and a
-	ld a, e
-	jr nz, .next
-; If the fade-counter is non-zero, we don't change the audio ROM bank because
-; it's needed to keep playing the music as it fades out. The FadeOutAudio
-; routine will take care of copying [wAudioSavedROMBank] to [wAudioROMBank]
-; when the music has faded out.
-	ld [wAudioROMBank], a
-.next
-	ld [wAudioSavedROMBank], a
-	scf
-	ret
+INCLUDE "audio/music/violetcity.asm"
+INCLUDE "audio/music/route29.asm"
+INCLUDE "audio/music/halloffame.asm"
+INCLUDE "audio/music/healpokemon.asm"
+INCLUDE "audio/music/evolution.asm"
+INCLUDE "audio/music/printer.asm"
 
-PlayMusic::
-	ld b, a
-	ld [wNewSoundID], a
-	xor a
-	ld [wAudioFadeOutControl], a
-	ld a, c
-	ld [wAudioROMBank], a
-	ld [wAudioSavedROMBank], a
-	ld a, b
-	jr PlaySound
 
-Func_2223::
-	xor a
-	ld [wChannelSoundIDs + Ch5], a
-	ld [wChannelSoundIDs + Ch6], a
-	ld [wChannelSoundIDs + Ch7], a
-	ld [wChannelSoundIDs + Ch8], a
-	ld [rNR10], a
-	ret
+SECTION "Songs 4", ROMX
 
-StopAllMusic::
-	ld a, $FF
-	ld [wNewSoundID], a
-; plays music specified by a. If value is $ff, music is stopped
-PlaySound::
-	push hl
-	push de
-	push bc
-	ld b, a
-	ld a, [wNewSoundID]
-	and a
-	jr z, .next
-	xor a
-	ld [wChannelSoundIDs + Ch5], a
-	ld [wChannelSoundIDs + Ch6], a
-	ld [wChannelSoundIDs + Ch7], a
-	ld [wChannelSoundIDs + Ch8], a
-.next
-	ld a, [wAudioFadeOutControl]
-	and a ; has a fade-out length been specified?
-	jr z, .noFadeOut
-	ld a, [wNewSoundID]
-	and a ; is the new sound ID 0?
-	jr z, .done ; if so, do nothing
-	xor a
-	ld [wNewSoundID], a
-	ld a, [wLastMusicSoundID]
-	cp $ff ; has the music been stopped?
-	jr nz, .fadeOut ; if not, fade out the current music
-; If it has been stopped, start playing the new music immediately.
-	xor a
-	ld [wAudioFadeOutControl], a
-.noFadeOut
-	xor a
-	ld [wNewSoundID], a
-	call DetermineAudioFunction
-	jr .done
+INCLUDE "audio/music/viridiancity.asm"
+INCLUDE "audio/music/celadoncity.asm"
+INCLUDE "audio/music/wildpokemonvictory.asm"
+INCLUDE "audio/music/successfulcapture.asm"
+INCLUDE "audio/music/gymleadervictory.asm"
+INCLUDE "audio/music/mtmoonsquare.asm"
+INCLUDE "audio/music/gym.asm"
+INCLUDE "audio/music/pallettown.asm"
+INCLUDE "audio/music/profoakspokemontalk.asm"
+INCLUDE "audio/music/profoak.asm"
+INCLUDE "audio/music/lookrival.asm"
+INCLUDE "audio/music/aftertherivalfight.asm"
+INCLUDE "audio/music/surf.asm"
+INCLUDE "audio/music/nationalpark.asm"
+INCLUDE "audio/music/azaleatown.asm"
+INCLUDE "audio/music/cherrygrovecity.asm"
+INCLUDE "audio/music/unioncave.asm"
+INCLUDE "audio/music/johtowildbattle.asm"
+INCLUDE "audio/music/johtowildbattlenight.asm"
+INCLUDE "audio/music/johtotrainerbattle.asm"
+INCLUDE "audio/music/lookyoungster.asm"
+INCLUDE "audio/music/tintower.asm"
+INCLUDE "audio/music/sprouttower.asm"
+INCLUDE "audio/music/burnedtower.asm"
+INCLUDE "audio/music/mom.asm"
+INCLUDE "audio/music/victoryroad.asm"
+INCLUDE "audio/music/pokemonlullaby.asm"
+INCLUDE "audio/music/pokemonmarch.asm"
+INCLUDE "audio/music/goldsilveropening.asm"
+INCLUDE "audio/music/goldsilveropening2.asm"
+INCLUDE "audio/music/lookhiker.asm"
+INCLUDE "audio/music/lookrocket.asm"
+INCLUDE "audio/music/rockettheme.asm"
+INCLUDE "audio/music/mainmenu.asm"
+INCLUDE "audio/music/lookkimonogirl.asm"
+INCLUDE "audio/music/pokeflutechannel.asm"
+INCLUDE "audio/music/bugcatchingcontest.asm"
 
-.fadeOut
-	ld a, b
-	ld [wLastMusicSoundID], a
-	ld a, [wAudioFadeOutControl]
-	ld [wAudioFadeOutCounterReloadValue], a
-	ld [wAudioFadeOutCounter], a
-	ld a, b
-	ld [wAudioFadeOutControl], a
-.done
-	pop bc
-	pop de
-	pop hl
-	ret
 
-GetNextMusicByte::
-	ld a, [H_LOADEDROMBANK]
-	push af
-	ld a, [wAudioROMBank]
-	call BankswitchCommon
-	ld d, $0
-	ld a, c
-	add a
-	ld e, a
-	ld hl, wChannelCommandPointers
-	add hl, de
-	ld a, [hli]
-	ld e, a
-	ld a, [hld]
-	ld d, a
-	ld a, [de]
-	inc de
-	ld [hl], e
-	inc hl
-	ld [hl], d
-	ld e, a
-	pop af
-	call BankswitchCommon
-	ld a, e
-	ret
+SECTION "Songs 5", ROMX
 
-InitMusicVariables::
-	push hl
-	push de
-	push bc
-	homecall Audio2_InitMusicVariables
-	pop bc
-	pop de
-	pop hl
-	ret
+INCLUDE "audio/music/mobileadaptermenu.asm"
+INCLUDE "audio/music/buenaspassword.asm"
+INCLUDE "audio/music/lookmysticalman.asm"
+INCLUDE "audio/music/crystalopening.asm"
+INCLUDE "audio/music/battletowertheme.asm"
+INCLUDE "audio/music/suicunebattle.asm"
+INCLUDE "audio/music/battletowerlobby.asm"
+INCLUDE "audio/music/mobilecenter.asm"
 
-InitSFXVariables::
-	push hl
-	push de
-	push bc
-	homecall Audio2_InitSFXVariables
-	pop bc
-	pop de
-	pop hl
-	ret
 
-StopAllAudio::
-	push hl
-	push de
-	push bc
-	homecall Audio2_StopAllAudio
-	pop bc
-	pop de
-	pop hl
-	ret
+SECTION "Extra Songs 1", ROMX
 
-DetermineAudioFunction::
-	ld a, [H_LOADEDROMBANK]
-	push af
-	ld a, [wAudioROMBank]
-	call BankswitchCommon
-; determine the audio function, based on the bank
-	cp BANK(Audio1_PlaySound)
-	jr nz, .checkForBank08
-; bank 02 (audio 1)
-	ld a, b
-	call Audio1_PlaySound
-	jr .done
+INCLUDE "audio/music/credits.asm"
+INCLUDE "audio/music/clair.asm"
+INCLUDE "audio/music/mobileadapter.asm"
 
-.checkForBank08
-	cp BANK(Audio2_PlaySound)
-	jr nz, .checkForBank1F
-; bank 08 (audio 2)
-	ld a, b
-	call Audio2_PlaySound
-	jr .done
 
-.checkForBank1F
-	cp BANK(Audio3_PlaySound)
-	jr nz, .bank20
-; bank 1f (audio 3)
-	ld a, b
-	call Audio3_PlaySound
-	jr .done
+SECTION "Extra Songs 2", ROMX
 
-.bank20
-; invalid banks will default to XX:6bd4
-; this is seen when encountering Missingno, as its sprite dimensions overflow to wAudioROMBank
-	ld a, b
-	call Audio4_PlaySound
-.done
-	pop af
-	call BankswitchCommon
-	ret
+INCLUDE "audio/music/postcredits.asm"
 
+
+SECTION "Custom Music", ROMX
+
+INCLUDE "audio/music/route47.asm"
+
+
+SECTION "Sound Effects", ROMX
+
+INCLUDE "audio/sfx.asm"
+
+
+SECTION "Crystal Sound Effects", ROMX
+
+INCLUDE "audio/sfx_crystal.asm"
+
+
+SECTION "Cries", ROMX
+
+INCLUDE "data/pokemon/cries.asm"
+
+INCLUDE "audio/cries.asm"
